249fdaa4678f619144467afd2d54ff5a60598ae3
Naoya Takenami <thriver@imac.local>
2020-06-22 17:59:13 +0900

    feat(top): Create module and component

---
+++ src/app/top/top-routing.module.ts
@@ -0,0 +1,17 @@
+import { TopComponent } from './top/top.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TopComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TopRoutingModule { }
